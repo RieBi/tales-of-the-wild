058b725d729386cfa98d1623dfc5d51ae9bbd422
RieBi <42877671+RieBi@users.noreply.github.com>
2024-06-16 12:20:31 +0200

    Add base player as template for all characters

---
+++ scenes/characters/player/player_base.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+
+@export var speed: int
+
+
+
+func _physics_process(delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var acceleration_time = 5
+	
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if direction:
+		velocity = velocity.move_toward(direction * speed, speed / acceleration_time)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, speed / acceleration_time)
+
+	move_and_slide()
