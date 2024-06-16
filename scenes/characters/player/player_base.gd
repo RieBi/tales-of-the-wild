55c7cfde0cb0795f4ae2f94fedecb9274c84a031
RieBi <42877671+RieBi@users.noreply.github.com>
2024-06-16 12:47:13 +0200

    Use roguelike tileset instead of packed

--- scenes/characters/player/player_base.gd
+++ scenes/characters/player/player_base.gd
@@ -8,7 +8,7 @@ extends CharacterBody2D
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var acceleration_time = 5
+	var acceleration_time: float = 5.0
 	
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if direction:
