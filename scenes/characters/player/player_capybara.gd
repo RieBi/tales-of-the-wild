cf1e021c62a2e50c71de2ce20124a02042893afd
RieBi <42877671+RieBi@users.noreply.github.com>
2024-06-16 19:04:16 +0200

    Add capybara playable character

---
+++ scenes/characters/player/player_capybara.gd
@@ -0,0 +1,21 @@
+extends PlayerBase
+@export var animated_sprite: AnimatedSprite2D
+
+func _ready() -> void:
+	super()
+	print("HELLO")
+
+func _physics_process(delta: float) -> void:
+	super(delta)
+	if velocity.x > 0:
+		animated_sprite.flip_h = true
+		animated_sprite.play("walk")
+	elif velocity.x < 0:
+		animated_sprite.flip_h = false
+		animated_sprite.play("walk")
+	elif velocity.y > 0:
+		animated_sprite.play("down")
+	elif velocity.y < 0:
+		animated_sprite.play("up")
+	else:
+		animated_sprite.play("idle")
